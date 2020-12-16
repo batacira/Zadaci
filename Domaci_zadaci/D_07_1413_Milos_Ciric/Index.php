537d6b7603e8f3283a50df2ec8d5a7832ec15d19
batacira <batacira@gmail.com>
2020-12-16 18:03:43 +0100

    Domaci zadatak sedam

---
+++ Domaci_zadaci/D_07_1413_Milos_Ciric/Index.php
@@ -0,0 +1,44 @@
+<!--  Zadatak 1 -->
+<?php
+    $farenhajt = 150;
+    $kelvin = 350;
+    $celzijusF = ($farenhajt - 32) * 5/9;
+    $celzijusK = $kelvin - 273.15; 
+    echo "<br>";
+    $kelvinOdFarenhajt = $celzijusF + 273.15;
+    $farenhajtOdKelvina = $celzijusK / (5/9) + 32;
+    echo $celzijusF;
+    echo "<br>";
+    echo $celzijusK;
+    echo "<br>";
+    echo $kelvinOdFarenhajt;
+    echo "<br>";
+    echo $farenhajtOdKelvina;
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    
+    // Zadatak 2
+    $n = 12;
+    $a = 4;
+    $a = $a + 2;
+    $ostalo = $n - $a;
+    echo $ostalo;
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    //Zadatak 3
+    $p = 1000;
+    $m = 2000;
+    $k = 1300;
+    $dz = ($p + $m - $k) / 2; 
+    $pkusur = $p - $dz;
+    $mkusur = $m - $dz;
+    echo $pkusur;
+    echo "<br>";
+    echo $mkusur;
+    
+?>
